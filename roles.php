b550ec63df518f399c946453709c7b99b9d6d76a
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2016-09-28 15:43:51 +0100

    Refactor otm_documents_add_roles() - inherit capabilities from WP core roles

--- roles.php
+++ roles.php
@@ -5,23 +5,29 @@
  */
 register_activation_hook( 'otm-documents/otm-documents.php', 'otm_documents_add_roles' );
 function otm_documents_add_roles() {
-	$member_capabilities = array(
-		'read_private_posts' => true,
-		'read_private_pages' => true
+	$editor_capabilities  = get_role( 'editor' )->capabilities;
+	$manager_capabilities = array_merge(
+		$editor_capabilities,
+		array(
+			'read_private_posts'   => true,
+			'list_users'           => true,
+			'create_users'         => true,
+			'add_users'            => true,
+			'edit_users'           => true,
+			'delete_users'         => true,
+			'remove_users'         => true,
+			'promote_users'        => true,
+			'manage_network_users' => true,
+		)
+	);
+	add_role( 'manager', __( 'Manager' ), $manager_capabilities );
+
+	$subscriber_capabilities = get_role( 'subscriber' )->capabilities;
+	$member_capabilities     = array_merge(
+		$subscriber_capabilities,
+		array(
+			'read_private_posts' => true
+		)
 	);
 	add_role( 'member', __( 'Member' ), $member_capabilities );
-
-	$manager_capabilities = array(
-		'read_private_posts'   => true,
-		'list_users'           => true,
-		'create_users'         => true,
-		'add_users'            => true,
-		'edit_users'           => true,
-		'delete_users'         => true,
-		'remove_users'         => true,
-		'promote_users'        => true,
-		'manage_network_users' => true,
-	);
-	$manager_capabilities = array_merge( $manager_capabilities, get_role( 'editor' )->capabilities );
-	add_role( 'manager', __( 'Manager' ), $manager_capabilities );
 }
