8d96c89b67de73c741b8c420e875d2ec9a204e0b
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2016-11-16 11:59:21 +0000

    Remove taxonomy customisations. (Leave that for the theme)

--- taxonomies.php
+++ taxonomies.php
@@ -1,42 +1,6 @@
 <?php
 
 /**
- * Change the position of the Event meta box
- */
-add_action( 'do_meta_boxes', 'otm_documents_change_event_meta_box_position' );
-function otm_documents_change_event_meta_box_position( $post_type ) {
-	$tax_name = 'event';
-
-	if ( $post_type != 'document' ) {
-		return;
-	}
-
-	remove_meta_box( $tax_name . 'div', $post_type, 'side' );
-
-	$taxonomy = get_taxonomy( $tax_name );
-	if ( ! $taxonomy->show_ui || false === $taxonomy->meta_box_cb ) {
-		return;
-	}
-
-	add_meta_box( $tax_name . 'box', $taxonomy->labels->singular_name, $taxonomy->meta_box_cb, null, 'advanced', 'core',
-		array( 'taxonomy' => $tax_name ) );
-}
-
-/**
- * Order Events by term_id
- * so that the last event added will be on the top of the list
- */
-add_filter( 'get_terms_args', 'otm_documents_reorder_events_by_term_id', 10, 2 );
-function otm_documents_reorder_events_by_term_id( $args, $taxonomies ) {
-	if ( 'event' == $taxonomies[0] ) {
-		$args['orderby'] = 'term_id';
-		$args['order']   = 'DESC';
-	}
-
-	return $args;
-}
-
-/**
  * Add taxonomy filter on admin page - Select element
  */
 add_action( 'restrict_manage_posts', 'otm_documents_add_taxonomy_filter' );
